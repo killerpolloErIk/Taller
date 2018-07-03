1fd46da58dc0c71f4af0b32344b08445b39175f4
Erik perez miramontes <erik.halo18@hotmail.com>
2018-07-03 12:18:31 -0500

    Primera Version

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+package.estructuradedatos;
+
+public.class.HolaMundo.{
+       public.static.void.main(String[]args{
+              System.out.println("Hola taller");
+              new java.util.Scanner(System.in).nextLine();
+}
+}
